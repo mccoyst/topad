b2a3a3632fd88907ef6cea2a834222239e2b815a
Chris Sexton <chrissexton@users.noreply.github.com>
2021-10-07 18:57:58 -0400

    modernize topad
    * Added go.mod
    * Added flag to change URL, and updated default URL

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mccoyst/topad
+
+go 1.17
--- main.go
+++ main.go
@@ -12,13 +12,18 @@ package main
 
 import (
 	"errors"
+	"flag"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
 )
 
+var serviceURL = flag.String("url", "https://pad.mccoy.space", "URL to pad service")
+
 func main() {
+	flag.Parse()
+
 	text, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
 		os.Stderr.WriteString("Failed to read anything: " + err.Error() + "\n")
@@ -31,12 +36,12 @@ func main() {
 	skipRedirect := errors.New("I want the Location")
 
 	c := http.Client{
-		CheckRedirect: func(req *http.Request, via []*http.Request) error{
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
 			return skipRedirect
 		},
 	}
 
-	resp, err := c.PostForm("http://itsapad.appspot.com", vals)
+	resp, err := c.PostForm(*serviceURL, vals)
 	if err != nil && err.(*url.Error).Err != skipRedirect {
 		os.Stderr.WriteString("Request failed: " + err.Error() + "\n")
 		os.Exit(1)
